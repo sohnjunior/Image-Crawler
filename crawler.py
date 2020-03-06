4b22d7894dce08923e2cabb0e0ddf5a9f3c4c8ee
handhand <sohnjunior@naver.com>
2020-03-06 14:26:56 +0900

    상태표시바 추가 및 이미지 소스 탐색로직 변경

--- crawler.py
+++ crawler.py
@@ -3,29 +3,56 @@ import json
 import urllib.request
 from selenium import webdriver
 
+from tqdm import tqdm
+
+import re
+import base64
+from io import BytesIO
+from PIL import Image
+
+
+def get_image_from_base64(codec):
+    """ convert base64 to image """
+    base64_data = re.sub('^data:image/.+;base64,', '', codec)
+    img = Image.open(BytesIO(base64.b64decode(base64_data)))
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    return img
+
 
 class Crawler:
-
-    # constructor
+    """
+    Google Web Image Crawler
+    """
     def __init__(self):
         self.keyword = str(input('keyword... : '))  # image keyword for searching
         self.dirPath = ""                           # image stored directory
 
-    # create directory for download if it is not exist
-    def createNewDirectory(self):
-        cwd = os.getcwd()
-        self.dirPath = os.path.join(cwd, self.keyword)
+    def create_new_directory(self):
+        """
+        Create directory for download if it is not exist
+        :return: None
+        """
+        self.cwd = os.getcwd()
+        self.dirPath = os.path.join(self.cwd, self.keyword)
 
         if not os.path.exists(self.dirPath):
             os.mkdir(self.dirPath)
 
-    # create the url path
-    def createURL(self):
+    def create_url(self):
+        """
+        Create the url path
+        :return: url for searching
+        """
         url = 'https://www.google.com/search?q=' + self.keyword + '&source=lnms&tbm=isch'
         return url
 
-    # search from chrome browser
-    def searchURL(self, url):
+    def search_url(self, url):
+        """
+        Search from chrome browser
+        :param url: search url
+        :return: webdriver
+        """
         # ========== headless driver options ========== #
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('headless')
@@ -36,7 +63,7 @@ class Crawler:
         # ============================================= #
 
         # if you don't want headless driver, remove chrome_option argument
-        browser = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
+        browser = webdriver.Chrome(os.path.join(self.cwd,'chromedriver'), chrome_options=chrome_options)
         browser.get(url)
         print(url)
         # scroll twice by 10000px
@@ -45,36 +72,43 @@ class Crawler:
 
         return browser
 
-    # download the image
-    def downloadImage(self, browser):
-        elements = browser.find_elements_by_xpath('//div[contains(@class,"rg_meta")]')
+    def download_image(self, browser):
+        """
+        Download the image
+        :param browser: google webdriver
+        :return: None
+        """
+        elements = browser.find_elements_by_xpath('//img[contains(@class,"rg_i")]')
         element_size = len(elements)  # used for progress status
 
-        for idx, element in enumerate(elements):
+        for idx in tqdm(range(element_size), bar_format='{l_bar}{bar:20}{r_bar}{bar:-10b}'):
+            save_path = self.dirPath + '/' + str(idx) + '.jpg'
+            img_src = elements[idx].get_attribute('src')
+            if img_src is None:
+                img_src = elements[idx].get_attribute('data-src')
 
-            img = json.loads(element.get_attribute('innerHTML'))["ou"]
-            img_type = json.loads(element.get_attribute('innerHTML'))["ity"]
-
-            try:
-                raw_img = urllib.request.urlopen(img).read()
-                filePath = self.dirPath + '/' + str(idx) + '.' + img_type
-
-                with open(filePath, mode="wb") as file:
-                    file.write(raw_img)
-            except:
-                pass  # ignore exception
-
-            # display progress status every 10 items
-            if idx % 10 == 0:
-                print('{}%  completed...'.format(idx * 100 / element_size))
+            if str(img_src).startswith('data:image'):
+                img = get_image_from_base64(img_src)
+                img.save(save_path, 'JPEG')
+            else:
+                try:
+                    urllib.request.urlretrieve(img_src, save_path)
+                except Exception as e:
+                    print('exception: ', idx, e)
 
         browser.close()  # close the browser
 
-    # main routines
     def run(self):
-        self.createNewDirectory()           # 1. create the directory
-        url = self.createURL()              # 2. create the path
-        browser = self.searchURL(url)       # 3. search image
-        self.downloadImage(browser)         # 4. download image
+        """
+        main routines
+        :return:
+        """
+        self.create_new_directory()           # 1. create the directory
+        url = self.create_url()              # 2. create the path
+        browser = self.search_url(url)       # 3. search image
+        self.download_image(browser)         # 4. download image
 
 
+if __name__ == '__main__':
+    newCrawler = Crawler()  # create new crawler
+    newCrawler.run()
